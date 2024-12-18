5915122e7f5c674bd14fc7e6bb733d8ec448d0aa
gchaewon <conjs1@naver.com>
2024-12-18 17:22:14 +0900

    :sparkles: GlobalException 적용

---
+++ src/main/java/com/pedalgenie/vote/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.pedalgenie.vote.exception;
+
+import com.pedalgenie.vote.global.ResponseTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ResponseTemplate<Object>> handleCustomException(CustomException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+
+        return ResponseTemplate.createTemplate(
+                errorCode.getHttpStatus(),
+                false,
+                errorCode.getMessage(),
+                null
+        );
+    }
+
+    // 정의한 예외 외에는 500 서버 에러처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseTemplate<Object>> handleGeneralException(Exception ex) {
+        // 로그 출력
+        ex.printStackTrace();
+
+        return ResponseTemplate.createTemplate(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                false,
+                "서버 오류가 발생했습니다..",
+                null
+        );
+    }
+}
